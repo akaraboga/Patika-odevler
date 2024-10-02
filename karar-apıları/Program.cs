aecd96945ecdde720dd5d82c0041a6525c0a3f62
akaraboga <a.karabogaa46@gmail.com>
2024-10-02 23:05:24 +0300

    karar yapıları odevi

--- karar-apıları/Program.cs
+++ karar-apıları/Program.cs
@@ -10,7 +10,30 @@ namespace karar_apıları
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Bir sayi giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi > 10)
+            {
+                Console.WriteLine("sayimiz 10 dan büyüktür");
+            }
+            else if (sayi < 10)
+            {
+                Console.WriteLine("sayimiz 10 ' dan küçüktür");
+            }
+            else {
+
+                Console.WriteLine("sayimiz 10 ' a eşittir");
+            }
+
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("sayimiz çift sayidir");
+            }
+            else
+            {
+                Console.WriteLine("tek sayidir");
+            }
 
             Console.ReadLine();
         }
